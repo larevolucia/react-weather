acfa84ba3acb3189bd38ea762027aae2f8e5c4c7
larevolucia <reis.lucia@gmail.com>
2022-11-21 21:33:42 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import Search from "./Search";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
       <Search />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="FooterS">
+      This site is open source
+      <div>
+        <a href="https://github.com/larevolucia/react-weather">Weather App</a>
+      </div>
+    </footer>
+  );
+}
