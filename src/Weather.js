f70205b354dad05308a99dbd189aa7c3ec7c16a4
larevolucia <reis.lucia@gmail.com>
2022-11-28 20:00:08 +0100

    animated icons/external component

--- src/Weather.js
+++ src/Weather.js
@@ -2,10 +2,9 @@ import React from "react";
 import "./Weather.css";
 import FormattedDate from "./FormattedDate";
 import WeatherInfo from "./WeatherInfo";
+import WeatherIcon from "./WeatherIcon";
 
 export default function Weather(props) {
-  const icon = `http://openweathermap.org/img/wn/${props.data.icon}@2x.png`;
-
   if (props.data.ready) {
     return (
       <div className="Weather border rounded-2 p-2">
@@ -15,12 +14,7 @@ export default function Weather(props) {
             <FormattedDate date={props.data.date} />
           </div>
           <div className="col-2 text-end">
-            <img
-              src={icon}
-              alt={props.data.description}
-              className="img-fluid"
-              width="80"
-            />
+            <WeatherIcon icon={props.data.icon} size={52} />
           </div>
           <div className="col-10 text-start">
             <WeatherInfo
