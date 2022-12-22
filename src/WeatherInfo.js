8b2b915a8483ee233fed8274baacf572e6a69eae
larevolucia <reis.lucia@gmail.com>
2022-12-22 14:21:44 +0100

    Revert "Added Forecast for Next day"
    This reverts commit a3c3a0a7cd647279f13e871138ff19a04e0fe5ef.

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -24,7 +24,7 @@ export default function WeatherInfo(props) {
             </a>
           </span>
         </div>
-        <div className="col-6 text-start">
+        <div className="col-6">
           <ul className="mt-2">
             <li className="text-capitalize">{props.description}</li>
             <li>Wind: {Math.round(props.wind)} km/h</li>
