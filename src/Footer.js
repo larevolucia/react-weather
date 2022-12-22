b4c6ddee57d2340977ab6bfc1f1285a12bcf0e54
larevolucia <reis.lucia@gmail.com>
2022-12-22 20:00:46 +0100

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="Footer">
       <div className="text-center mt-3">
-        ✌️ This website was coded by Lúcia Reis, and is
+        This website was coded by Lúcia Reis, and is
         <a
           href="https://github.com/larevolucia/react-weather"
           target="_blank"
