af82e69eaa68954ba21a66e8dc0cdd35bd71db37
larevolucia <reis.lucia@gmail.com>
2024-06-20 11:04:00 +0200

    useEffect dependency

--- src/Search.js
+++ src/Search.js
@@ -31,7 +31,7 @@ export default function Search() {
       }
       initialFetch.current = true;
     }
-  }, []);
+  }, [fetchWeatherByCoordinates]);
 
   useEffect(() => {
     if (errorAlert.display) {
