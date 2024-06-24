0956c20105c4128ca662481b827164961f0e98a0
larevolucia <reis.lucia@gmail.com>
2024-06-24 11:03:57 +0200

    used localStorage to save pre-selected unit on new search

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,11 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/Weather.css";
 import FormattedDate from "./FormattedDate";
 import WeatherInfo from "./WeatherInfo";
 import WeatherForecast from "./WeatherForecast";
 
 export default function Weather({ data }) {
-  const [unit, setUnit] = useState("celsius");
+  const [unit, setUnit] = useState(() => {
+    const savedUnit = localStorage.getItem("weatherUnit");
+    return savedUnit ? savedUnit : "celsius";
+  });
+
+  useEffect(() => {
+    localStorage.setItem("weatherUnit", unit);
+  }, [unit]);
 
   function handleUnitChange(newUnit) {
     setUnit(newUnit);
